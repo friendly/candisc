78a2a2551415393d1806889ede4ae839360b85a4
friendly <friendly@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-21 14:18:50 +0000

    Added vectors()
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/candisc/pkg@39 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/cancor.R
+++ R/cancor.R
@@ -179,15 +179,4 @@ coef.cancor <- function(object, type=c("x", "y", "both", "list"), ...) {
 		)
 }
 
-# utility for drawing labeled vectors
-# TODO: handle xpd=TRUE somewhere so labels aren't cut off
-vectors <- function(x, labels=rownames(x), scale=1, length=.1, angle=10, col="blue", pos=NULL, ...) {
-	x <- scale*x
-	arrows(0, 0, x[,1], x[,2], length=length, angle=angle, col=col, ...)
-	if (!is.null(labels)) {
-		if(missing(pos)) pos <- ifelse(x[,1]>0, 4, 2)
-		# DONE: position labels relative to arrow ends (outside)
-		text(x[,1], x[,2], labels, pos=pos, col=col, ...)
-		}
-}
 
---
+++ R/vectors.R
@@ -0,0 +1,15 @@
+# utility for drawing labeled vectors
+# TODO: handle xpd=TRUE somewhere so labels aren't cut off
+# DONE: allow origin to be a two-col matrix like x
+# TODO: calculate default length in terms of par("usr")
+
+vectors <- function(x, origin=c(0,0), labels=rownames(x), scale=1, length=.1, angle=10, col="blue", pos=NULL, ...) {
+	x <- scale*x
+	if (is.vector(origin)) origin <- matrix(origin, ncol=2)
+	arrows(origin[,1], origin[,2], x[,1], x[,2], length=length, angle=angle, col=col, ...)
+	if (!is.null(labels)) {
+		if(missing(pos)) pos <- ifelse(x[,1]>0, 4, 2)
+		# DONE: position labels relative to arrow ends (outside)
+		text(x[,1], x[,2], labels, pos=pos, col=col, ...)
+		}
+}
