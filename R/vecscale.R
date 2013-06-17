530ff68773ba976b205dc3838a9da8e1563ce181
friendly <friendly@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-06-17 13:29:37 +0000

    bumped version number
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/candisc/pkg@70 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/vecscale.R
+++ R/vecscale.R
@@ -1,5 +1,11 @@
 #' Scale vectors to fill the current plot
 
+#' @param vectors a two-column matrix giving the end points of a collection of vectors
+#' @param bbox the bounding box of the containing plot region within which the vectors are to be plotted
+#' @param origin origin of the vectors
+#' @param factor maximum length of the rescaled vectors
+#' @return scale factor 
+
 vecscale <- function(vectors, 
 		bbox=matrix(par("usr"), 2, 2),
 		origin=c(0, 0), factor=0.95) {	
