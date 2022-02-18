741c2b9b09c3ae2fc585685914e6d9f181cc3d33
Michael Friendly <friendly@yorku.ca>
2022-02-18 16:57:13 -0500

    styler some files

--- R/can_lm.R
+++ R/can_lm.R
@@ -9,23 +9,27 @@
 #' @param ... Arguments passed to \code{\link{candisc}}
 #' @return A \code{lm} object if \code{term} is a rank 1 hypothesis, otherwise a \code{mlm} object
 
-can_lm   <- function(mod, term, ...) {
-#  require(candisc)
+can_lm <- function(mod, term, ...) {
   if (!(inherits(mod, "mlm"))) stop("model must be a mlm")
   term.names <- gsub(" ", "", labels(terms(mod)))
   which.term <- which(term == term.names)
-  if (length(which.term)==0) stop(term, " does not appear in the model")
-  
+  if (length(which.term) == 0) stop(term, " does not appear in the model")
+
   can <- candisc(mod, term, ...)
-#  term <- mod$term                        # term for which candisc was done
-  lm.terms <- can$terms                   # terms in original lm
+  #  term <- mod$term                        # term for which candisc was done
+  lm.terms <- can$terms # terms in original lm
   scores <- can$scores
-  resp <- if (can$rank==1) "Can1" else
-          paste("cbind(", paste("Can",1:can$rank,sep="", collapse = ","), ")")
-  txt <- paste("lm(", resp, " ~ ",
-              paste( lm.terms, collapse = "+"), ", data=scores)" )
-  can.mod <- eval(parse(text=txt))
+
+  resp <- if (can$rank == 1) {
+    "Can1"
+  } else {
+    paste("cbind(", paste("Can", 1:can$rank, sep = "", collapse = ","), ")")
+  }
+
+    txt <- paste(
+    "lm(", resp, " ~ ",
+    paste(lm.terms, collapse = "+"), ", data=scores)"
+  )
+  can.mod <- eval(parse(text = txt))
   can.mod
 }
-
-
